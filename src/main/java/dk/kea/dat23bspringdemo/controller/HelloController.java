f7c8e910b8692cae4dff0ee7aad1d52959190450
jarltuxen <jarl.tuxen@gmail.com>
2024-02-09 09:33:31 +0100

    erdetx

--- src/main/java/dk/kea/dat23bspringdemo/controller/HelloController.java
+++ src/main/java/dk/kea/dat23bspringdemo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package dk.kea.dat23bspringdemo.controller;
 
+import dk.kea.dat23bspringdemo.service.FridayCalculator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,18 @@ public class HelloController {
     @GetMapping("/parameter")
     public String parameter(@RequestParam String inddata){
         return inddata + " er dagens tekst!";
-
     }
+
+    @GetMapping("/erdetfredag")
+    public String erdetfredag(){
+        FridayCalculator fridayCalculator = new FridayCalculator();
+        return fridayCalculator.erdetfredag();
+    }
+
+    @GetMapping("/erdetx")
+    public String erDetX(@RequestParam int dag){
+        FridayCalculator fridayCalculator = new FridayCalculator();
+        return fridayCalculator.erDetX(dag);
+    }
+
 }
---
+++ src/main/java/dk/kea/dat23bspringdemo/service/FridayCalculator.java
@@ -0,0 +1,66 @@
+package dk.kea.dat23bspringdemo.service;
+
+import java.time.LocalDate;
+
+public class FridayCalculator {
+
+    //FridayCalculator branch
+    public String erdetfredag(){
+        LocalDate today = LocalDate.now();
+        int q = today.getDayOfMonth(); //day of the month (1-31)
+        int m = today.getMonthValue(); //the month (1-12)
+
+
+        //month 1 and 2 count as month 13 and 14 the previous year
+        int y = today.getYear(); //the year
+        if (m<3){
+            m+=12; //convert to month 13 or 14
+            y--; //convert to previous year
+        }
+
+        int j = y / 100; //the century
+        int k = y % 100; //the year of the century
+
+        int h = (int) ((q + Math.floor(13 * (m+1)/5) + k + Math.floor(k/4) + Math.floor(j/4) - 2 * j) % 7);
+
+        int d = ((h + 5) % 7) + 1; //day of the week
+
+        //ternary operator: expression ? true value : false value
+        //return d == 5 ? "Yes!":"No!";
+        if (d==5){
+            return "Yes!";
+        }else{
+            return "No!";
+        }
+    }
+
+    public String erDetX(int x){
+        LocalDate today = LocalDate.now();
+        int q = today.getDayOfMonth(); //day of the month (1-31)
+        int m = today.getMonthValue(); //the month (1-12)
+
+
+        //month 1 and 2 count as month 13 and 14 the previous year
+        int y = today.getYear(); //the year
+        if (m<3){
+            m+=12; //convert to month 13 or 14
+            y--; //convert to previous year
+        }
+
+        int j = y / 100; //the century
+        int k = y % 100; //the year of the century
+
+        int h = (int) ((q + Math.floor(13 * (m+1)/5) + k + Math.floor(k/4) + Math.floor(j/4) - 2 * j) % 7);
+
+        int d = ((h + 5) % 7) + 1; //day of the week
+
+        //ternary operator: expression ? true value : false value
+        //return d == 5 ? "Yes!":"No!";
+        if (d==x){
+            return "Yes!";
+        }else{
+            return "No!";
+        }
+    }
+
+}
